5657b6a19f98902f53e2ba81747f70948636b7f1
Dmytro Vaskiv <dimavaskiv9@gmail.com>
2025-07-04 03:29:38 +0300

    commm

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,7 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age >= 0 ? <p className="Person__age">I am {person.age}</p> : ''}
+    {person.age != undefined ? <p className="Person__age">I am {person.age}</p> : ''}
     {person.isMarried ? (
       <p className="Person__partner">
         {`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
